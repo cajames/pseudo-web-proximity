58d49586ce8096cb4927b55cf5b03e5343583ffe
Chris James <chris@webglowit.net>
2018-06-26 21:45:54 +1000

    Initial commit with vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <h1>Hello world</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {}
+})
+export default class App extends Vue {}
+</script>
